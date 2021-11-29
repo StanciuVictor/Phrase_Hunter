34e1f33b37d97d5c75e5fe07622bf5743fce1098
StanciuVictor <victor.stanciu@yahoo.com>
2021-11-29 16:26:37 +0200

    Update app.js
    Added event listeners to start new game and check letters typed.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+let game;
+
+const resetBtn = document.getElementById('btn__reset');
+const qwerty = document.getElementById('qwerty');
+const keyboard = document.querySelectorAll('.keyrow button');
+
+// Start a new game when user clicks the 'Start Game' button
+resetBtn.addEventListener('click', () => {
+  game = new Game();
+  game.startGame();
+  // console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+});
+
+// Listen for clicked letters
+qwerty.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    game.handleInteraction(e.target);
+  }
+});
+
+// Listen for typed keys
+window.addEventListener('keyup', (e) => {
+  [...keyboard].forEach(key => {
+    if (key.textContent === e.key) {
+      game.handleInteraction(key);
+    }
+  });
+});
